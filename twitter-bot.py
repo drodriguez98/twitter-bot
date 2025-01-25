d4e5a370085631cdf91616f8561379305d69f643
Diego Rodríguez <diego25598@gmail.com>
2025-01-25 21:24:54 +0100

    update texts and rename functions

--- twitter-bot.py
+++ twitter-bot.py
@@ -55,9 +55,9 @@ def download_image(url, save_path):
                     file.write(chunk)
             downloaded_urls.add(url) # Add the URL to the set after downloading
         else:
-            print(f"Error al descargar la imagen de {url}")
+            print(f"Error downloading image from {url}")
     else:
-        print(f"La imagen de {url} ya ha sido descargada.")
+        print(f"Image from {url} already downloaded.")
 
 def download_video(url, save_path):
     if url not in downloaded_urls: # Check if the URL has already been downloaded
@@ -72,9 +72,9 @@ def download_video(url, save_path):
             os.remove(temp_path)
             downloaded_urls.add(url) # Add the URL to the set after downloading
         else:
-            print(f"Error al descargar el video de {url}")
+            print(f"Error downloading video from {url}")
     else:
-        print(f"El video de {url} ya ha sido descargado.")
+        print(f"Video from {url} already downloaded.")
 
 def convert_to_mp4(input_path, output_path):
     try:
@@ -110,26 +110,26 @@ def fetch_memes_from_reddit():
             download_video(post.url, save_path)
 
 # Feature to post a tweet with the path of the downloaded file
-def publicar_tweet(texto):
+def publish_tweet(texto):
     try:
         response = client.create_tweet(text=texto)
-        print(f"Tweet publicado: {response.data}")
+        print(f"Tweet published: {response.data}")
         return True
     except Exception as e:
-        print(f"Error al publicar el tweet: {e}")
+        print(f"Error posting tweet: {e}")
         return False
 
 # Function to move files to uploads folder
-def mover_a_uploads(archivos):
-    for archivo in archivos:
-        ruta_origen = os.path.join(DOWNLOADS_DIR, archivo)
-        ruta_destino = os.path.join(UPLOADS_DIR, archivo)
-        shutil.move(ruta_origen, ruta_destino)
-        print(f"Archivo movido a uploads: {archivo}")
+def move_to_uploads(files):
+    for file in files:
+        origin_path = os.path.join(DOWNLOADS_DIR, file)
+        destination_path = os.path.join(UPLOADS_DIR, file)
+        shutil.move(origin_path, destination_path)
+        print(f"File moved to uploads: {file}")
 
 # Main function to execute all tasks
-def ejecutar_bot():
-    print("Ejecutando bot...")
+def run_bot():
+    print("Running bot...")
 
     # Download memes every 15 seconds
     schedule.every(15).seconds.do(fetch_memes_from_reddit)
@@ -139,17 +139,17 @@ def ejecutar_bot():
         schedule.run_pending()
         
         # Check files in the downloads folder
-        archivos = [f for f in os.listdir(DOWNLOADS_DIR) if f.endswith(('jpg', 'jpeg', 'png', 'gif', 'mp4', 'mov'))]
+        files = [f for f in os.listdir(DOWNLOADS_DIR) if f.endswith(('jpg', 'jpeg', 'png', 'gif', 'mp4', 'mov'))]
         
         # Publish only if there are at least 5 files
-        if len(archivos) >= 5:
-            for archivo in archivos[:5]: # Only the first 5 files found are published
-                texto = f"Ruta del archivo: {os.path.join(DOWNLOADS_DIR, archivo)}"
-                if publicar_tweet(texto):
-                    mover_a_uploads([archivo]) # Move file to uploads after publishing  
+        if len(files) >= 5:
+            for file in files[:5]: # Only the first 5 files found are published
+                texto = f"Ruta del file: {os.path.join(DOWNLOADS_DIR, file)}"
+                if publish_tweet(texto):
+                    move_to_uploads([file]) # Move file to uploads after publishing  
                     time.sleep(15) # Wait 15 seconds between tweets
 
         time.sleep(10)  # Check the folder every 10 seconds
 
 if __name__ == "__main__":
-    ejecutar_bot()
+    run_bot()
